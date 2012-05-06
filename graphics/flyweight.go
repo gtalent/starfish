0254dc9c9cac7fc34d53128a92de242ea99a0857
Gary Talent <gtalent2@gmail.com>
2012-05-05 22:36:13 -0500

    Made flyweight handle concurrent access better.

--- graphics/flyweight.go
+++ graphics/flyweight.go
@@ -15,9 +15,13 @@
 */
 package graphics
 
-import "sync"
+import (
+	"sync"
+	"time"
+)
 
 type flynode struct {
+	sync.Mutex
 	key     key
 	val     interface{}
 	clients int
@@ -50,31 +54,56 @@ func newFlyweight(loader func(key) interface{}, unloader func(key, interface{}))
 }
 
 func (me *flyweight) checkout(key key) interface{} {
-	me.lock.Lock()
-	node, ok := me.items[key.String()]
-	if !ok {
-		node = new(flynode)
-		node.key = key
-		node.val = me.loader(key)
-		node.clients = 0
-		me.items[key.String()] = node
+	for {
+		node, ok := me.items[key.String()]
+		if !ok {
+			node = new(flynode)
+			node.key = key
+			node.val = me.loader(key)
+			node.clients = 1
+
+			me.lock.Lock()
+			if n, ok := me.items[key.String()]; ok {
+				me.lock.Unlock()
+				n.Lock()
+				if n.clients == 0 {
+					n.Unlock()
+					time.Sleep(0)
+					continue
+				}
+				n.clients++
+				n.Unlock()
+				return n.val
+			}
+			me.items[key.String()] = node
+			me.lock.Unlock()
+			return node.val
+		} else {
+			node.Lock()
+			if node.clients == 0 {
+				node.Unlock()
+				time.Sleep(0)
+				continue
+			}
+			node.clients++
+			node.Unlock()
+			return node.val
+		}
 	}
-	node.clients++
-	me.lock.Unlock()
-	return node.val
+	return nil
 }
 
 func (me *flyweight) checkin(key key) {
-	me.lock.Lock()
 	n := me.items[key.String()]
 	if n == nil {
 		me.lock.Unlock()
 		return
 	}
+	n.Lock()
 	n.clients--
 	if n.clients == 0 {
 		delete(me.items, key.String())
 		me.unloader(key, n.val)
 	}
-	me.lock.Unlock()
+	n.Unlock()
 }
